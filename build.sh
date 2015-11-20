d04d35bd9904fb6138f67180f3c4fdde0f523c15
Kazuhisa Yamamoto <ak.hisashi@gmail.com>
2015-11-20 15:22:00 +0900

    First commit.

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+rm -f myfunc.zip
+zip -r myfunc.zip index.js github.yml node_modules
---
+++ index.js
@@ -0,0 +1,79 @@
+yaml = require('js-yaml');
+fs   = require('fs');
+url_util = require('url')
+
+var github = require('octonode');
+var setting = yaml.safeLoad(fs.readFileSync('github.yml'));
+
+exports.handler = function(event, context) {
+  // Getting '_ball' label lists.
+  var ball_tags = function(){
+    if ((event.action == 'created' || event.action == 'opened') == false){
+      return null;
+    }
+    if (event.action == 'created' && 'issue' in event){
+      return create_tags_from_message(event.comment.body); //add issue and pullreq comment.
+    } else if (event.action == 'opened' && 'issue' in event){
+      return create_tags_from_message(event.issue.body); //create issue.
+    } else if (event.action == 'opened' && 'pull_request' in event){
+      return create_tags_from_message(event.pull_request.body); //create pullreq.
+    } else {
+      return [];
+    }
+  }
+
+  // Getting issue number
+  var number = function(){
+    if ((event.action == 'created' || event.action == 'opened') == false){
+      return null;
+    }
+    if ('issue' in event){
+      return event.issue.number;
+    } else if ('pull_request' in event){
+      return event.pull_request.number;
+    } else {
+      return null;
+    }
+  }
+
+  // create tags from message.
+  var create_tags_from_message = function(){
+    var list = message.match(/@[a-z0-9_-]+/i);
+    return list.map(function(v){ return v.replace(/@/,'') + '_ball'});
+  }
+
+  var repos = function(){
+    var url = null;
+    if ('issue' in event){
+      url = event.issue.url;
+    } else if ('pull_request' in event){
+      url = event.pull_request.url;
+    }
+    if (url == null){
+      return null
+    }
+    var list = url_util.parse(url).path.split('/');
+    return splitted_path[2] + '/' + splitted_path[3];
+  }
+
+  var updateLabel = function(labels, number){
+    var client = github.client(setting.token);
+    var ghissue = client.issue(repos(), number);
+    ghissue.update(
+      {'labels': labels},
+      function(){
+        context.succeed('complete!');
+      }
+    )
+  }
+
+  var num = number();
+  if (num == null){
+    context.succeed('not updated.');
+  }
+  var tags = ball_tags();
+  if (tags.length == 0){
+    context.succeed('not updated.');
+  }
+  updateLabel(tags, num);
+}
