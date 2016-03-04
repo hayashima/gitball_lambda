e766295148d5fc8c3cd7f7a0ec203fcbf42a51ab
Kazuhisa Yamamoto <ak.hisashi@gmail.com>
2016-03-04 23:35:56 +0900

    statusのテストを追加

--- gitball.js
+++ gitball.js
@@ -1,11 +1,11 @@
-function status(){
+exports.status = function(){
   if (this.action == 'created' && 'issue' in this){
     return 'created_issue';
   } else if (this.action == 'opened' && 'issue' in this){
     return 'opend_issue';
   } else if (this.action == 'opened' && 'pull_request' in this){
-    return 'opend_pull_request'
+    return 'opend_pull_request';
   } else {
     return null;
   }
-}
+};
--- test/gitball_spec.js
+++ test/gitball_spec.js
@@ -1,8 +1,41 @@
 var assert = require("assert");
+var gitball = require('../gitball');
 
-describe('テスト', function(){
-  it('Hello mocha', function(){
-    var msg = 'mocha';
-    assert.equal('Hello ' + msg, 'Hello mocha');
-  })
-})
+describe('#status', function(){
+  var event;
+  describe('created issue', function(){
+    before(function(){
+      event = {action: 'created', issue: 'some errors'};
+    });
+    it('created_issue', function(){
+      assert.equal(gitball.status.call(event), 'created_issue');
+    });
+  });
+
+  describe('opend issue', function(){
+    before(function(){
+      event = {action: 'opened', issue: 'some errors'};
+    });
+    it('opend_issue', function(){
+      assert.equal(gitball.status.call(event), 'opend_issue');
+    });
+  });
+
+  describe('opend pull request', function(){
+    before(function(){
+      event = {action: 'opened', pull_request: 'fixed errors'};
+    });
+    it('opend_pull_request', function(){
+      assert.equal(gitball.status.call(event), 'opend_pull_request');
+    });
+  });
+
+  describe('other', function(){
+    before(function(){
+      event = {};
+    });
+    it('return null', function(){
+      assert.equal(gitball.status.call(event), null);
+    });
+  });
+});
